8c9d80d274824511492dcd4765b3d1d2a659bbd8
Денис Пьянков <91528031+DenisPyankov@users.noreply.github.com>
2021-09-29 22:30:23 +0500

    Add files via upload

---
+++ 1ENT.js
@@ -0,0 +1,30 @@
+let fs = require('fs');
+let argv = process.argv;
+let alph = new Array();
+let data;
+let i = 0 , n = 0;
+let entropy = 0;
+
+data = fs.readFileSync(argv[2],'utf8');
+data.toString();
+for (i = 0; i < data.length; i++) {
+	alph[data.charAt(i)] = 0;
+}
+for (i = 0; i < data.length; i++) {
+	alph[data.charAt(i)]++;
+}
+for (i in alph) {
+	alph[i] /= data.length;
+	n++;
+}
+if (n == 1) {
+	entropy = 0;
+}
+else {
+	for (i in alph) {
+		entropy += alph[i]*(Math.log(alph[i])/Math.log(n));
+	}
+	entropy *= (-1);
+}
+console.log(entropy);
+
